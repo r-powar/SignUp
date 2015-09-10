60bf5f0fabe3a9114da263c9411a27f65d36174d
Raj Powar <rpowar@Rajs-Air.lan>
2015-09-09 20:41:01 -0700

    form submission

--- signUp.js
+++ signUp.js
@@ -2,23 +2,24 @@ $(document).ready(function(){
 	
 	/*TODO 
 		Modernizer for date type
-		Validation on form submission 
+		Center main div
 	*/
-
+	var validity = false,
+		validField = false;
 	$('#inputEmail').on('blur', function(){
-		signUp.usernameValid();
+		validField = signUp.usernameValid(validity);
 	});
 
 	$('#inputPassword').on('blur', function(){
-		signUp.passwordValidity();
+		validField = signUp.passwordValidity(validity);
 	});
 
 	$('#inputConfirmPassword').on('blur', function(){
-		signUp.cnfrmPassword();
+		validField = signUp.cnfrmPassword(validity);
 	});
 
 	$('#inputFirstName').on('blur', function(){
-		signUp.nameValidity();
+		validField = signUp.nameValidity(validity);
 	});
 
 	$('#inputLastName').on('blur', function(){
@@ -26,44 +27,47 @@ $(document).ready(function(){
 	});
 
 	$('#inputDOB').on('blur', function(){
-		signUp.checkDOB();
+		validField = signUp.checkDOB(validity);
 	});
 
+
 	$("form").submit(function(event){
-		signUp.init();
-		console.log('test');
-		event.preventDefault();
+		var formChildrenAncestor = $(this).find('input').parent().parent().not('.inputLastName'),
+			formChildren = $(this).find('input').not('#inputLastName');
+		formChildren.each(function(i){
+			if(formChildren.eq(i).val() == ""){
+				validField = false;
+			}
+		});
+
+		if(!validField){
+			formChildrenAncestor.each(function(i){
+				formChildrenAncestor.eq(i).addClass('has-error')
+			});
+			event.preventDefault();
+		}else{
+			signUp.convertDate($('#inputDOB').val());
+		}
 	});
 }); 
 
 var signUp = {
-	init: function(){
-		this.usernameValid();
-		this.passwordValidity();
-		this.cnfrmPassword();
-		this.nameValidity();
-		this.lastNameField();
-		this.checkDOB();
-		this.convertDate();
-	},
 
-	checkValidity:function(){
-
-	},
-
-	usernameValid: function(){
+	usernameValid: function(validity){
 		var emailValue = $('#inputEmail').val(),
 			pattern = new RegExp(/^((([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|((\x22)((((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(([\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(\\([\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(\x22)))@((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.?$/i),
 			regexVal = pattern.test(emailValue);
 		if(emailValue == "" || !regexVal){
 			$('.inputEmail').removeClass("has-success").addClass("has-error");
 		}else{
-
+			validity = true
 			$('.inputEmail').removeClass("has-error").addClass("has-success");
 		}
+
+		return validity;
 	},
 
-	passwordValidity: function(){
+	passwordValidity: function(validity){
 		var passwordVal = $('#inputPassword').val();
 
 		if(passwordVal == ""){
@@ -72,11 +76,14 @@ var signUp = {
 			$('.inputPassword').addClass("has-error").removeClass("has-success");
 			$('#inputPassword').attr("placeholder", "Password must be at least 6 characters");
 		}else{
+			validity = true;
 			$('.inputPassword').addClass("has-success").removeClass("has-error");
 		}
+
+		return validity;
 	},
 
-	cnfrmPassword: function(){
+	cnfrmPassword: function(validity){
 		var confirmPassVal = $('#inputConfirmPassword').val(),
 			passwordVal = $('#inputPassword').val();
 
@@ -84,12 +91,14 @@ var signUp = {
 			$('.inputConfirmPassword').addClass("has-error").removeClass("has-success");
 			$('#inputConfirmPassword').attr("placeholder", "Password did not match");
 		}else{
+			validity = true;
 			$('.inputConfirmPassword').addClass("has-success").removeClass("has-error");
 		}
 
+		return validity;
 	},
 
-	nameValidity: function(){
+	nameValidity: function(validity){
 		var firstName = $('#inputFirstName').val(),
 			pattern = new RegExp(/^[a-zA-Z]*$/),
 			regexVal = pattern.test(firstName);
@@ -100,8 +109,11 @@ var signUp = {
 			$('.inputFirstName').addClass("has-error").removeClass("has-success");
 			$('#inputFirstName').attr("placeholder", "Only letters for this field");
 		}else{
+			validity = true;
 			$('.inputFirstName').addClass("has-success").removeClass("has-error");
 		}
+
+		return validity;
 	},
 
 	lastNameField: function(){
@@ -112,7 +124,7 @@ var signUp = {
 		}
 	},
 
-	checkDOB: function(){
+	checkDOB: function(validity){
 		var dateOfBirth = $('#inputDOB').val(),
 			today = new Date(),
 			birthdate = new Date(dateOfBirth),
@@ -126,8 +138,11 @@ var signUp = {
     	if(dateOfBirth == "" || (age < 14 || age > 150)){
     		$('.inputDOB').addClass("has-error").removeClass("has-success");
     	}else{
+    		validity = true;
     		$('.inputDOB').addClass("has-success").removeClass("has-error");
     	}
+
+    	return validity;
 	},
 
 	convertDate: function(dateOfBirth){
